9af7e55388b9ad2a3afe35bef1d579cb3024466c
kirby@puppetlabs.com <kirby@puppetlabs.com>
2013-12-10 18:38:08 -0800

    Adding define and associated templates to backup postgres

---
+++ manifests/postgresql_backup.pp
@@ -0,0 +1,48 @@
+# Sample Usage:
+#
+#   postgresql_backup { 'my application':
+#     db_host     => 'postgres.example.com',
+#     db_pass     => 'db pass',
+#     db_user     => 'postgres',
+#     db_name     => 'postgres',
+#     backup_path => '/backups',
+#   }
+#
+define postgresql_backup (
+  $db_host,
+  $db_pass,
+  $db_user,
+  $db_name,
+  $backup_path,
+  $group,
+  $owner,
+  $mode,
+) {
+  case $ensure {
+    present: {
+
+    file { "/usr/local/bin/${name}_backup":
+      ensure => present,
+      group  => $group,
+      owner  => $owner,
+      mode   => '0755',
+      source => template('postgres_backup/postgres_backup.erb')
+    }
+
+    file { "/etc/${name}_backup.conf":
+      ensure  => present,
+      group   => $group,
+      owner   => $owner,
+      mode    => '0600',
+      content => template('postgresbackup/postgres_backup.conf.erb')
+    }
+
+    file { "$owner/.pgpass":
+      ensure  => present,
+      group   => $group,
+      owner   => $owner,
+      mode    => '0600',
+      content => template('postgres_backup/pgpass.erb')
+    }
+  }
+}
