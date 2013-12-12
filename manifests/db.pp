8bd49f2b5d5706ed72fefd3560f31afc7ce78b00
kirby@puppetlabs.com <kirby@puppetlabs.com>
2013-12-11 16:24:00 -0800

    Using concat instead of managing a file resource

--- manifests/db.pp
+++ manifests/db.pp
@@ -4,7 +4,7 @@
 #     db_host     => 'postgres.example.com',
 #     db_pass     => 'db pass',
 #     db_user     => 'postgres',
-#     db_name     => 'postgres',
+#     db_title     => 'postgres',
 #     backup_path => '/backups',
 #   }
 #
@@ -12,13 +12,14 @@ define postgresql_backup::db (
   $db_host,
   $db_pass,
   $db_user,
-  $db_name,
+  $db_title,
   $backup_path,
   $group  = 'root',
   $owner  = 'root',
   $ensure = present,
+  $pgpass = '/root/.pgpass',
 ) {
-  file { "/usr/local/bin/${name}_backup":
+  file { "/usr/local/bin/${title}_backup":
     ensure => $ensure,
     group  => $group,
     owner  => $owner,
@@ -26,19 +27,23 @@ define postgresql_backup::db (
     source => template('postgresql_backup/postgresql_backup.erb')
   }
 
-  file { "/etc/${name}_backup.conf":
-    ensure  => present,
+  file { "/etc/${title}_backup.conf":
+    ensure  => $ensure,
     group   => $group,
     owner   => $owner,
     mode    => '0600',
     content => template('postgresql_backup/postgresql_backup.conf.erb')
   }
 
-  file { "$owner/.pgpass":
-    ensure  => present,
-    group   => $group,
-    owner   => $owner,
-    mode    => '0600',
-    content => template('postgresql_backup/pgpass.erb')
+  concat { $pgpass:
+    owner => $owner,
+    group => $group,
+    mode  => '0600'
+  }
+
+  concat::fragment { $title:
+    target  => $pgpass,
+    content => "\n${db_host}:5432:${db_name}:${db_user}:${db_pass}\n",
+    order   => '1'
   }
 }
